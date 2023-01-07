c50ba7a563e8e82824eb114ffbe25f9e1a9d2e11
paixaols <lucas.lsop@gmail.com>
2023-01-07 16:39:13 -0300

    Update models

--- invest/models/forms.py
+++ invest/models/forms.py
@@ -1,17 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import BooleanField, DateTimeField, StringField, PasswordField
+from wtforms.validators import InputRequired, ValidationError
 
 
 class RegisterForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    name = StringField('name', validators=[InputRequired()])
+    email = StringField('email', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
     confirm_password = PasswordField(
-        'confirm_password', validators=[DataRequired()]
+        'confirm_password', validators=[InputRequired()]
     )
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    email = StringField('email', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
+
+
+def datetime_validator(form, field):
+    if not form.unexpirable.data and field.data is None:
+        raise ValidationError('Datetime validation error')
+
+
+class AssetForm(FlaskForm):
+    name = StringField('name', validators=[InputRequired()])
+    description = StringField('description', validators=[InputRequired()])
+    market = StringField('market', validators=[InputRequired()])
+    asset_type = StringField('asset_type', validators=[InputRequired()])
+    asset_group = StringField('asset_group', validators=[InputRequired()])
+    expiration_date = DateTimeField(
+        'expiration_date', 
+        format='%d/%m/%Y', 
+        validators=[datetime_validator]
+    )
+    unexpirable = BooleanField('unexpirable')
--- invest/models/tables.py
+++ invest/models/tables.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from werkzeug.security import generate_password_hash
 
 from invest import db
@@ -7,12 +8,14 @@ class User(db.Model):
     __tablename__ = 'users'
     __table_args__ = {'schema': 'invest'}
 
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
     pw_hash = db.Column(db.String, nullable=False)
-    member_since = db.Column(db.DateTime, nullable=False)
-    active_since = db.Column(db.DateTime, nullable=False)
+    registration_date = db.Column(db.DateTime, nullable=False)
+    activation_date = db.Column(db.DateTime, nullable=False)
+    access_expiration_date = db.Column(db.DateTime, nullable=False)
+    persona = db.Column(db.String(16), nullable=False)
 
     @property
     def is_active(self):
@@ -27,14 +30,48 @@ class User(db.Model):
         return False
 
     def get_id(self):
-        return str(self.id)
+        return str(self.user_id)
 
-    def __init__(self, name, email, password, member_since, active_since):
+    def __init__(
+            self, name, email, password, registration_date, activation_date,
+            persona='user'
+            ):
         self.name = name
         self.email = email
         self.pw_hash = generate_password_hash(password)
-        self.member_since = member_since
-        self.active_since = active_since
+        self.registration_date = registration_date
+        self.activation_date = activation_date
+        self.access_expiration_date = activation_date + timedelta(days=365)
+        self.persona = persona
 
     def __repr__(self):
         return f'<User {self.email}>'
+
+
+class Asset(db.Model):
+    __tablename__ = 'assets'
+    __table_args__ = {'schema': 'invest'}
+
+    asset_id = db.Column(db.Integer, primary_key=True)
+    asset_name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(128), nullable=False)
+    market = db.Column(db.String(128), nullable=False)
+    asset_type = db.Column(db.String(128), nullable=False)
+    asset_group = db.Column(db.String(128), nullable=False)
+    asset_expiration_date = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, name, description, market, asset_type, group, expiration_date):
+        self.asset_name = name
+        self.description = description
+        self.market = market
+        self.asset_type = asset_type
+        self.asset_group = group
+        self.asset_expiration_date = expiration_date
+
+    def __repr__(self):
+        return f'<Asset {self.asset_name}>'
+
+
+# class Wallet(db.Model):
+#     __tablename__ = 'wallets'
+#     __table_args__ = {'schema': 'invest'}
