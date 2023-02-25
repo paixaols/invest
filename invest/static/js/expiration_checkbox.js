bbb8b6b593ac3ac339d1c2d2361b3fd667a300be
paixaols <lucas.lsop@gmail.com>
2023-02-25 11:04:35 -0300

    Change framework to django

--- invest/static/js/expiration_checkbox.js
+++
@@ -1,9 +0,0 @@
-function checkboxAction(){
-    var checkbox = document.getElementById('myCheck')
-    var text = document.getElementById('myInput')
-    if(checkbox.checked == true){
-        text.disabled = true
-    } else {
-        text.disabled = false
-    }
-}
