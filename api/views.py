20e8fab421976adbc5cbb36bb9d4cca94cafe7ed
paixaols <lucas.lsop@gmail.com>
2023-03-19 20:46:27 -0300

    Add: endpoint get_assets filtra resultados & vários endpoints ordenam os resultados alfabeticamente

--- api/views.py
+++ api/views.py
@@ -10,24 +10,29 @@ from invest.models import Asset, AssetGroup, AssetType, Market
 
 @api_view(['GET'])
 def get_assets(request):
-    assets = Asset.objects.all()
+    data = request.GET
+    data = { k: int(v) for k, v in data.items() }
+    if data:
+        assets = Asset.objects.filter(**data).order_by('name')
+    else:
+        assets = Asset.objects.all().order_by('name')
     serializer = AssetSerializer(assets, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def get_asset_groups(request):
-    groups = AssetGroup.objects.all()
+    groups = AssetGroup.objects.all().order_by('group')
     serializer = AssetGroupSerializer(groups, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def get_asset_types(request):
-    types = AssetType.objects.all()
+    types = AssetType.objects.all().order_by('type')
     serializer = AssetTypeSerializer(types, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def get_markets(request):
-    markets = Market.objects.all()
+    markets = Market.objects.all().order_by('name')
     serializer = MarketSerializer(markets, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
