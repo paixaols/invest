1876d51e4819cbc8d22590df6f30e7cbcdcec882
Lucas Paixão <lucas.lsop@gmail.com>
2023-05-17 21:20:40 -0300

    Add: endpoint para baixar dados do yahoo finance.

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('get-asset-groups/', views.get_asset_groups, name='get_asset_groups'),
     path('get-asset-types/', views.get_asset_types, name='get_asset_types'),
     path('get-markets/', views.get_markets, name='get_markets'),
+    path('price-history/<str:code>/', views.price_history, name='price_history'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
+import yfinance as yf
+
+from datetime import datetime, timedelta
+from django.utils import timezone
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -36,3 +40,27 @@ def get_markets(request):
     markets = Market.objects.all().order_by('name')
     serializer = MarketSerializer(markets, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def price_history(request, code):
+    data = request.GET
+    if data.get('end'):
+        end_date = datetime.strptime(data.get('end'), '%Y-%m-%d')
+    else:
+        end_date = timezone.now()
+    if data.get('start'):
+        start_date = datetime.strptime(data.get('start'), '%Y-%m-%d')
+    else:
+        start_date = end_date-timedelta(days=7)
+
+    data = (
+        yf.download(
+            code,
+            start=start_date,
+            end=end_date
+        )['Close']
+    )
+    data = data.reset_index().rename(columns={'Date':'date', 'Close':'close'})
+    data['date'] = data['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+
+    return Response(data.to_json(orient='records'), status=status.HTTP_200_OK)
